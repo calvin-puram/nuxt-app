8e09d44a5f36a2628b6850525e3c2381ad3269f3
calvin <puram.calvin@gmail.com>
2020-05-05 18:46:53 +0100

    added nested route

---
+++ pages/events/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id;
+    }
+  },
+  head() {
+    return {
+      title: 'All About Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'all the gist about event #' + this.id
+        }
+      ]
+    };
+  }
+};
+</script>
