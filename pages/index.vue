a01d986e4a856f99c338f0897a8d73bf2f26dbba
calvin <puram.calvin@gmail.com>
2020-05-05 21:22:42 +0100

    added axios call

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,28 @@
     <h1>
       Events
     </h1>
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+      data-index="event.id"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard';
 export default {
+  components: {
+    EventCard
+  },
+  asyncData({ $axios }) {
+    return $axios.get('http://localhost:4000/events/').then(res => {
+      return {
+        events: res.data
+      };
+    });
+  },
   head() {
     return {
       title: 'Events '
