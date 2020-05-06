05395c2dc862934e43016e8931851c1a24768c55
calvin <puram.calvin@gmail.com>
2020-05-06 16:05:09 +0100

    added meta to the post page

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -4,6 +4,23 @@
   </div>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      title: 'All Articles',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'All post articles'
+        }
+      ]
+    };
+  }
+};
+</script>
+
 <style scoped>
 .posts-page {
   display: flex;
