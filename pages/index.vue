7d20e2cbdeea9f4fa62dadd4c25741c8459a5882
calvin <puram.calvin@gmail.com>
2020-05-06 16:00:18 +0100

    added a meta to welcome page

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,16 @@
   </div>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      title: 'Latest Articles'
+    };
+  }
+};
+</script>
+
 <style scoped>
 .intro {
   height: 300px;
