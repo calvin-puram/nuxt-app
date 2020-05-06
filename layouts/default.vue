659bca80f1f033a188946a152a307fa66996ce76
calvin <puram.calvin@gmail.com>
2020-05-06 15:58:43 +0100

    added default meta for layout page

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,23 @@
   </div>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      titleTemplate: '%s - Technical blog post',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Read great articles about what is happening in tech world'
+        }
+      ]
+    };
+  }
+};
+</script>
+
 <style>
 html {
   font-family: 'Open Sans', sans-serif;
